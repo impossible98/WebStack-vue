4add75aaa84eccea554b39678edd08f6278eaf2f
Chris <42108459+impossible98@users.noreply.github.com>
2021-08-06 18:24:28 +0800

    feat: upgrade

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import router from './router/index';
 import App from './App.vue'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
 
 import './assets/css/fonts/linecons/css/linecons.css'
 import './assets/css/fonts/fontawesome/css/font-awesome.min.css'
@@ -12,19 +10,10 @@ import './assets/css/xenon-components.css'
 import './assets/css/xenon-skins.css'
 import './assets/css/nav.css'
 
-Vue.config.productionTip = false
-Vue.use(VueRouter)
 
-const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About }
-]
-
-const router = new VueRouter({
-  routes
-})
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-  router
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount('#app');
