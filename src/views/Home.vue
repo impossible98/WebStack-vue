ad6c34b258f9d452ffefefea137135c745cf79ec
Chris <42108459+impossible98@users.noreply.github.com>
2021-08-07 05:40:47 +0800

    feat: upgrade

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,29 @@
   <div class="page-container">
     <div class="main-content">
       <div v-for="(item, idx) in items" :key="idx">
-        <div v-if="item.web">
-          <WebItem :item="item" :transName="transName" />
-        </div>
-        <div v-else v-for="(subItem, idx) in item.children" :key="idx">
-          <WebItem :item="subItem" :transName="transName" />
+        <div>
+          <div>
+            <h4 class="text-gray">
+              <i class="linecons-tag" :id="item"></i>{{ item.name }}
+            </h4>
+            <div class="row">
+              <div class="col-sm-3" v-for="(web, idx) in item.web" :key="idx">
+                <div
+                  class="xe-widget xe-conversations box2 label-info"
+                  title=""
+                  @click="openweb(web.url)"
+                  data-toggle="tooltip"
+                  data-placement="bottom"
+                  :data-original-title="web.url"
+                >
+                  <div class="xe-comment-entry">
+                    <img :src="web.logo" class="lozad img-circle" width="40" />
+                    <p>{{ web.title }}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -14,30 +32,21 @@
 </template>
 
 <script>
-import WebItem from '../components/WebItem.vue'
-import itemsData from '../../config/data.json'
+import itemsData from "../../config/data.json";
 
 export default {
-  name: 'Index',
-  components: {
-    WebItem
-  },
+  name: "Index",
+
   data() {
     return {
       items: itemsData,
-      lang: {},
-      langList: [
-        {key: 'zh', name: '简体中文', flag: './assets/images/flags/flag-cn.png'}
-      ]
-    }
+    };
   },
-  created() {
-    this.lang = this.langList[0]
-  },
+  created() {},
   methods: {
-    transName(webItem) {
-      return this.lang.key === 'en' ? webItem.en_name : webItem.name;
-    }
-  }
-}
+    openweb(url) {
+      window.open(url, "_blank");
+    },
+  },
+};
 </script>
