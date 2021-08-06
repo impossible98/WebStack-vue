9f628476d527fae1af39d03a1ed8bd7d0e300154
Chris <42108459+impossible98@users.noreply.github.com>
2021-08-07 05:45:27 +0800

    feat: upgrade

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,24 +3,22 @@
     <div class="main-content">
       <div v-for="(item, idx) in items" :key="idx">
         <div>
-          <div>
-            <h4 class="text-gray">
-              <i class="linecons-tag" :id="item"></i>{{ item.name }}
-            </h4>
-            <div class="row">
-              <div class="col-sm-3" v-for="(web, idx) in item.web" :key="idx">
-                <div
-                  class="xe-widget xe-conversations box2 label-info"
-                  title=""
-                  @click="openweb(web.url)"
-                  data-toggle="tooltip"
-                  data-placement="bottom"
-                  :data-original-title="web.url"
-                >
-                  <div class="xe-comment-entry">
-                    <img :src="web.logo" class="lozad img-circle" width="40" />
-                    <p>{{ web.title }}</p>
-                  </div>
+          <h4 class="text-gray">
+            <i class="linecons-tag" :id="item"></i>{{ item.name }}
+          </h4>
+          <div class="row">
+            <div class="col-sm-3" v-for="(web, idx) in item.web" :key="idx">
+              <div
+                class="xe-widget xe-conversations box2 label-info"
+                title=""
+                @click="openweb(web.url)"
+                data-toggle="tooltip"
+                data-placement="bottom"
+                :data-original-title="web.url"
+              >
+                <div class="xe-comment-entry">
+                  <img :src="web.logo" class="lozad img-circle" width="40" />
+                  <p>{{ web.title }}</p>
                 </div>
               </div>
             </div>
@@ -36,7 +34,6 @@ import itemsData from "../../config/data.json";
 
 export default {
   name: "Index",
-
   data() {
     return {
       items: itemsData,
